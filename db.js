711b580ee0fc125c707a1fa90a2574cb47cf7bd0
Manju145 <manjuyadav0514001@gmail.com>
2023-07-13 13:56:49 +0530

    backend

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose")
+require("dotenv").config()
+
+
+// const connection = mongoose.connect(process.env.mongoURL)
+const connection = mongoose.connect("mongodb://localhost:27017")
+
+
+module.exports={
+    connection
+}
+
+
+
