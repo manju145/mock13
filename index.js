c44f0c7c9dbd38fde86f20e157acc4e11e46d5e1
Manju145 <manjuyadav0514001@gmail.com>
2023-07-13 13:57:36 +0530

    done

--- index.js
+++ index.js
@@ -13,23 +13,23 @@ app.use("/users",userRouter)
 
 
 
-// app.listen(process.env.port,async()=>{
-//     try {
-//         await connection
-//         console.log("connected to the DB")
-//     } catch (error) {
-//         console.log({ msg: error.message });
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log("connected to the DB")
+    } catch (error) {
+        console.log({ msg: error.message });
         
-//     }
-//     console.log(`server is running at port ${process.env.port}`)
-// })
-
-
-app.listen(8080,async()=>{
-    try{
-        await connection;
-        console.log("Connection with db");
-    }catch(err){
-        console.log("server is running at port 8080");
     }
+    console.log(`server is running at port ${process.env.port}`)
 })
+
+
+// app.listen(8080,async()=>{
+//     try{
+//         await connection;
+//         console.log("Connection with db");
+//     }catch(err){
+//         console.log("server is running at port 8080");
+//     }
+// })
